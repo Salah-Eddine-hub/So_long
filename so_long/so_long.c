7d5fe2b243034b1114d0017037ee4011508ea1c9
Salah-eddine Harrachmin <sharrach@e2r10p5.1337.ma>
2022-04-02 16:23:17 +0000

    mlx add

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,11 +6,18 @@
 /*   By: sharrach <sharrach@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 13:35:46 by sharrach          #+#    #+#             */
-/*   Updated: 2022/03/23 17:56:42 by sharrach         ###   ########.fr       */
+/*   Updated: 2022/04/02 13:52:17 by sharrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+typedef struct	d_data{
+	int img_width;
+	int img_height;
+	char **map;
+
+}	s_truct;
 
 typedef struct	s_data {
 	void	*img;
@@ -22,25 +29,19 @@ typedef struct	s_data {
 
 int	main(void)
 {
+	s_truct	pic;
+	t_data	img;
+	char	*relative_path = "1.xpm";
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
-	
-	char **map;
 
-	map = read_map("map.ber");
-	// for (int i = 0; i < 7; i++)
-	// {
-	// 	printf("%s\n", map[i]);
-	// }
-	if (map_checker(map))
+	pic.map = read_map("map.ber");
+	if (map_checker(pic.map))
 		return (1);
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-	// 							&img.endian);
-	// my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_win = mlx_new_window(mlx, 50 * ft_strlen(pic.map[0]), 50 * ft_arrlen(pic.map), "so_long");
+	img.img = mlx_xpm_file_to_image(mlx, relative_path, &pic.img_width, &pic.img_height);
+	
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
